a0f11b67f8208ec39632a1e1e5a20d6b5d955925
brent-edwards-online <brent@brentedwardsonline.com>
2017-03-29 12:19:55 +1000

    Rename projects

---
+++ src/brentedwardsonline_identityserver/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityServerWithAspNetIdentity.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(150, ErrorMessage = "Last name cannot be longer than 150 characters.")]
+        public string LastName { get; set; }
+    }
+}
